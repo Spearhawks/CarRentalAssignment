5a818b730e20d3c7ed929f589b8a548e07bad8af
Karl Magnus Magnusson <karl.magnus.magnusson@gmail.com>
2023-09-21 08:26:28 +0200

    Uppdaterat listorna med mer data.

--- CarRental.Data/Classes/Data.cs
+++ CarRental.Data/Classes/Data.cs
@@ -16,7 +16,13 @@ public class Data : IData
     private void SeedData()
     {
         _persons.Add(new Customer { CustID = 1, FirstName = "John", LastName = "Doe" });
-        _vehicles.Add(new Car { RegistrationNo = "ABC123", Make ="Volvo", Odometer = 3233, CostPerKm = 120, CostPerDay = 300, VehicleType = VehicleType.Van , RentedStatus = RentedStatus.Available });
+        _persons.Add(new Customer { CustID = 1, FirstName = "John", LastName = "Doe" });
+        _persons.Add(new Customer { CustID = 1, FirstName = "John", LastName = "Doe" });
+        _vehicles.Add(new Car { RegistrationNo = "ABC123", Make = "Volvo", Odometer = 3233, CostPerKm = 120, CostPerDay = 300, VehicleType = VehicleType.Van, RentedStatus = RentedStatus.Available });
+        _vehicles.Add(new Car { RegistrationNo = "GGF332", Make = "Opel", Odometer = 3993, CostPerKm = 100, CostPerDay = 200, VehicleType = VehicleType.Sedan, RentedStatus = RentedStatus.Rented });
+        _vehicles.Add(new Car { RegistrationNo = "ABC123", Make = "Volvo", Odometer = 3233, CostPerKm = 120, CostPerDay = 300, VehicleType = VehicleType.Van, RentedStatus = RentedStatus.Available });
+        _bookings.Add(new Booking { RegistrationNo = "ABC123", Customer = null, KmRented = 3233, KmReturned = 3333, Rented = new DateOnly(), Returned = new DateOnly(), Cost = 300, Status = BookingStatus.Open });
+        _bookings.Add(new Booking { RegistrationNo = "GGF332", Customer = null, KmRented = 3993, KmReturned = 5677, Rented = new DateOnly(), Returned = new DateOnly(), Cost = 300, Status = BookingStatus.Closed });
         _bookings.Add(new Booking { RegistrationNo = "ABC123", Customer = null, KmRented = 3233, KmReturned = 3333, Rented = new DateOnly(), Returned = new DateOnly(), Cost = 300, Status = BookingStatus.Open });
     }
 
